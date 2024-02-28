5d484db18a9699222c6a8720b0d4b170f8821b11
Nightowl-code <22u1587@students.git.edu>
2024-02-28 22:43:38 +0530

    my first commit

---
+++ members/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from time import timezone
+
+# Create your models here.
+class SubmittedData(models.Model):
+    candidate = models.CharField(max_length=20,null=True)
+    start_time=models.CharField(max_length=30,null=True)
+    time=models.CharField(max_length=30,null=True)
+    list=models.CharField(max_length=60,null=True)
+
+    
+    def __str__(self):       
+        return self.candidate +" time taken: "+ self.time+" ==>> "+ self.list
---
+++ myproject/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path,include
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    # path("members/", include("django.contrib.auth.urls")),  # for authentication
+    path("", include("members.urls")),
+]
